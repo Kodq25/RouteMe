1b7b03d7b284c4e8d51096f46ecad1eacbcb16e0
elvis <elviscobbinah25@gmail.com>
2026-04-05 17:09:55 +0000

    infl

--- JAvaApp/app/src/main/java/com/example/myapp/ui/SearchFragment.java
+++ JAvaApp/app/src/main/java/com/example/myapp/ui/SearchFragment.java
@@ -7,9 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -58,13 +56,6 @@ public class SearchFragment extends Fragment {
 
     public SearchFragment() {
         super(R.layout.fragment_search);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
     @Override
